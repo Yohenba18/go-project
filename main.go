bb1d42eafd49d7619e3ccbbc68cb8cb818e48e2c
Yohenba18 <roddik12kshetri@gmail.com>
2021-10-23 10:00:06 +0530

    basic project

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func hello(c *fiber.Ctx)error{
+	return c.SendString("Hello, World!")
+}
+
+func main() {
+  app := fiber.New()
+
+  app.Get("/", hello)
+
+  app.Listen(":3000")
+}
